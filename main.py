a55c93c9c871790637ff6d76057badb5ae70b848
Nicolas Becker <nicolasbecker@nicolass-mbp.lojasrenner.com.br>
2017-08-01 13:50:27 -0300

    Update Main

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 logfile = open("honeynet_Feb.log", "r")
+aType = []
+aDest = []
 for line in logfile:
     line_split = line.split()
-    print line_split
+ #   print line_split
     list = line_split[5], line_split[6], line_split[12]
-    print list
+    aType.append(line_split[6]);
+    aDest.append(line_split[12])
+  #  print list
+
+print aType
+print aDest
